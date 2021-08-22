ad5f423300de86b093be3f10c64269a4f75abdbe
Adam Lancaster <lancaster28@hotmail.com>
2021-08-22 15:28:39 +0100

    mix new

---
+++ lib/ex_tractors.ex
@@ -0,0 +1,18 @@
+defmodule ExTractors do
+  @moduledoc """
+  Documentation for `ExTractors`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExTractors.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_tractors_test.exs
@@ -0,0 +1,8 @@
+defmodule ExTractorsTest do
+  use ExUnit.Case
+  doctest ExTractors
+
+  test "greets the world" do
+    assert ExTractors.hello() == :world
+  end
+end
